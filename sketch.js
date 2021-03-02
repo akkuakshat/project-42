acde3b6234d180fe2e8f5792ac0eec1bb39e3875
akkuakshat <71920085+akkuakshat@users.noreply.github.com>
2021-03-02 16:36:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+function setup() {
+  createCanvas(800,800);
+  createSprite(400, 200, 50, 50);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(255,255,255);  
+  background(200);
+  strokeWeight(4);
+  stroke("gold");
+  fill(255);
+  ellipse(400,400,680,680);
+
+
+  noStroke();
+  fill(0);
+  textSize(30);
+  text("12",400,150);
+  text("3",650,400); 
+  text("6",400,650); 
+  text("9",150,400);
+
+  text("1",540,200);   
+  text("2",620,283); 
+  text("4",620,513); 
+  text("5",540,600); 
+  text("11",260,200); 
+  text("10",180,280); 
+  text("8",180,513);
+  text("7",260,600);  
+
+
+
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  translate(400,400);
+  rotate(-70)
+  var scAngle = map(sc, 0 ,60,0,360);
+  var mnAngle = map(mn, 0 ,60,0,3160);
+  var hrAngle = map(hr%12,0,12,0,340);
+
+  
+  push();
+  rotate(scAngle);
+  stroke("blue");
+  strokeWeight(4);
+  line(0,0,200,0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke("green");
+  strokeWeight(4);
+  line(0,0,150,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke("red");
+  strokeWeight(4);
+  line(0,0,100,0);
+  pop();
+  drawSprites();
+}
